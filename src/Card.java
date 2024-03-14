61409a631109d13dbf67390528e1562aee7c7c4e
byron <cox00144@algonquinlive.com>
2024-03-13 22:24:25 -0400

    Add suit and Rank to card and flip card functionality.

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,47 @@
 public class Card {
 
+    private Suit suit;
+    private Rank rank;
+    // Image of Front and Back of Card
+    // private Image frontOfCard;
+    // private Image backOfCard;
+    private boolean isFaceDown;
+
+    public Card(Rank rank, Suit suit) {
+        this.suit = suit;
+        this.rank = rank;
+        isFaceDown = true;
+    }
+
+    public String getSuit() {
+        return suit.displaySuit();
+    }
+
+    public int getRank() {
+        return rank.getRank();
+    }
+
+    public boolean isFaceDown() {
+        return isFaceDown;
+    }
+
+    public void flipCard(){
+        // flips isFaceDown value to true and false
+        // flipping the card
+        isFaceDown = !isFaceDown;
+    }
+
+    @Override
+    public String toString(){
+        String cardText = "";
+        if(isFaceDown) {
+            cardText = "Card is Face Down";
+        } else {
+            cardText += rank.displayRank() + " of "
+                    + suit.displaySuit();
+        }
+        return cardText;
+    }
+
+
 }
