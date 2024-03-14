8bc6ef8dfd1d8f51b00f521d9e624577d2be89a0
byron <cox00144@algonquinlive.com>
2024-03-13 22:29:51 -0400

    Added ArrayList.

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
 
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        cards = new ArrayList<Card>();
+    }
+
+    public void emptyDeck() {
+        cards.clear();
+    }
 }
