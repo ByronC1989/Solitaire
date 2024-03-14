4f5bb38601f9cc0325117a70d67fe6593545f8ed
byron <cox00144@algonquinlive.com>
2024-03-13 21:50:40 -0400

    Created Suit Enum.

---
+++ src/Suit.java
@@ -0,0 +1,19 @@
+public enum Suit {
+    HEARTS("Hearts"),
+    SPADES("Spades"),
+    DIAMONDS("Diamonds"),
+    CLUBS("Clubs");
+
+    private final String suitString;
+
+    private Suit(String suitString){
+        // Constructor for Suit enum
+        this.suitString = suitString;
+    }
+
+    public String displaySuit(){
+        // access the value of the rank
+        return suitString;
+    }
+
+}
