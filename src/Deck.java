7065c05d3b234bc200cbcf7d008c713b4bb47f29
byron <cox00144@algonquinlive.com>
2024-03-20 18:57:01 -0400

    Added Deck Functions.

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,49 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Deck {
 
-    private ArrayList<Card> cards;
+    private final ArrayList<Card> cards;
 
     public Deck(){
+        // create Card Array
         cards = new ArrayList<Card>();
+
+        for(Suit suit: Suit.values()) {
+            for(Rank rank: Rank.values()) {
+                Card card = new Card(rank, suit);
+                card.flipCard(); // remove this after testing so that cards are facedown.
+                cards.add(card);
+            }
+        }
     }
 
     public void emptyDeck() {
         cards.clear();
+    }
+
+    public void shuffleDeck() {
+        // Use collections shuffle method come up with our own algorithm?
+        Collections.shuffle(cards);
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public String printDeck() {
+        String str = "";
+        for (Card c: cards){
+            str += c.toString() + "\n";
+        }
+        return str;
+    }
+
+    public Card drawCard(){
+        if (!cards.isEmpty()) {
+            return cards.removeFirst();
+        }
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        Card card = new Card(Rank.ACE, Suit.DIAMONDS);
-
         // flipping a card.
-        System.out.println(card);
-        card.flipCard();
-        System.out.println(card);
-        card.flipCard();
-        System.out.println(card);
+//        System.out.println(card);
+//        card.flipCard();
+//        System.out.println(card);
+//        card.flipCard();
+//        System.out.println(card);
+
+        // Create deck of cards
+        Deck deck = new Deck();
+
+        System.out.println("Deck Before Shuffling: ");
+        System.out.println(deck.printDeck());
+        System.out.println("\n Deck of Cards Contains: " + deck.getCards().size() + " Cards");
+
+        deck.shuffleDeck();
+
+        System.out.println("Deck After Shuffling: ");
+        System.out.println(deck.printDeck());
+
+
+        System.out.println(deck.drawCard() + " was drawn from the deck");
+        System.out.println("Deck After drawing a card: ");
+        System.out.println(deck.printDeck());
 
 
     }
