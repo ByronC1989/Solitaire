29434e76caddd571ddb79057755e6e5d0a19e12b
byron <cox00144@algonquinlive.com>
2024-03-25 20:30:54 -0400

    Testing draw Card method.

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,33 @@ public class Main {
         // Create deck of cards
         Deck deck = new Deck();
 
-        System.out.println("Deck Before Shuffling: ");
-        System.out.println(deck.printDeck());
-        System.out.println("\n Deck of Cards Contains: " + deck.getCards().size() + " Cards");
+//        System.out.println("Deck Before Shuffling: ");
+//        System.out.println(deck.printDeck());
+//        System.out.println("\n Deck of Cards Contains: " + deck.getCards().size() + " Cards");
 
         deck.shuffleDeck();
 
-        System.out.println("Deck After Shuffling: ");
-        System.out.println(deck.printDeck());
+//        System.out.println("Deck After Shuffling: ");
+//        System.out.println(deck.printDeck());
+//
+//
+//        System.out.println(deck.drawCard() + " was drawn from the deck");
+//        System.out.println("Deck After drawing a card: ");
+//        System.out.println(deck.printDeck());
 
 
-        System.out.println(deck.drawCard() + " was drawn from the deck");
-        System.out.println("Deck After drawing a card: ");
-        System.out.println(deck.printDeck());
+  // UNCOMMENT BELOW TO TEST DRAWING EVERY CARD FROM THE DECK!
 
+//        System.out.println("Deck contains: " + deck.deckSize() + " cards");
+//
+//        int numOfCards = 0;
+//
+//        while(deck.deckSize() > 0){
+//            System.out.println(deck.drawCard() + " was drawn from the deck");
+//            numOfCards++;
+//        }
+//
+//        System.out.println(numOfCards + " cards removed from the deck!");
 
     }
 }
