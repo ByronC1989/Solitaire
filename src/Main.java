cfb2f63800af4256f1ecfc304c8a0968ab7e3613
Alycia <alycia.curry01@gmail.com>
2024-05-13 14:37:19 -0400

    anotha one

--- src/Main.java
+++ src/Main.java
@@ -132,6 +132,7 @@ public class Main {
 //                } else {
 //                        System.out.println("Could not add Five of Hearts to the foundation.");
 //                }
+
 //                foundation.place(fiveOfH, Suit.HEARTS);
 //                foundation.printFoundation();
 
